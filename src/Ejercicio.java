7d387cc94ad34bc932a022ddcd2f6cb69cf16eae
aquarrion <garyaguilar300@gmail.com>
2021-11-27 13:26:46 -0400

    Maximo comun divisor (mcd)

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -12,4 +12,28 @@ public class Ejercicio {
 
         return factorial;
     }
+
+    /*
+    Maximo comun divisor (mcd), algoritmo de euclides
+    a = 412 y b = 184
+    * */
+
+    public static int mcd(int a, int b){
+        int mcd;
+
+        if (a == b){
+            //caso base
+            mcd = b;
+        }else{
+            if (a > b){
+                a = a - b;
+            }else{
+                b = b - a;
+            }
+
+            mcd = mcd(a, b);
+        }
+
+        return mcd;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Ejercicio.factorial(5));
-
+        //System.out.println(Ejercicio.factorial(5));
+        System.out.println(Ejercicio.mcd(412,184));
 
     }
 }
